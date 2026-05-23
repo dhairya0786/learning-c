b34d99ec4b0a3b232d88ce07fd4d76efc73c7955
dhairya0786 <dhairyasethi333@gmail.com>
2026-05-23 18:05:33 +0530

    Update mario.c

--- mario.c
+++ mario.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
-void print_row(int bricks) {
-    for (int i = 0; i < bricks; i++) {
-        printf("#");// Print a brick
-    }
-    printf("\n");
-}
-int main() {
-    int height;
-    printf("Height: ");
-    scanf("%d", &height);
 
-    for (int i = 0; i < height; i++) {
-        print_row(i + 1);// Print a row of bricks
-    }
+int main() {
+    
+   int cents;
+
+   do {
+      printf("Change owed: ");
+      scanf("%d", &cents);
+   } while (cents < 0);
+
+    int coins = 0;
+
+    coins += cents / 25;
+    cents = cents % 25;
+
+    coins += cents / 10;
+    cents = cents % 10;
+
+    coins += cents / 5;
+    cents = cents % 5;
+
+    coins += cents / 1;
+    cents = cents % 1;
+
+    printf("Number of coins needed: %d\n", coins);
 }
